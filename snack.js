9d67088701d503b8abf68d094cc50db05620f3d7
CarloMart88 <carlo.martino1988@gmail.com>
2025-10-29 11:53:54 +0100

    second snack

--- snack.js
+++ snack.js
@@ -16,3 +16,10 @@ const plus = (x , y) => x + y
 
 console.log(plus(1 , 2))
 
+/*🏆 Snack 2
+Crea una arrow function che calcola il quadrato di un numero.
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga. */
+
+const quadrato = (a) => a * a
+
+console.log(quadrato(2))
