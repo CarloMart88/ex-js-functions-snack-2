2da5a20bfcf02e6967dd1cf8f20476ae4f715f8f
CarloMart88 <carlo.martino1988@gmail.com>
2025-10-29 17:40:48 +0100

    fourth snack

--- snack.js
+++ snack.js
@@ -35,3 +35,18 @@ function eseguiOperazione(a , b ,operatore){
 const add = (a , b) => a + b
 
 console.log(eseguiOperazione(1 , 2 , add))
+
+
+/*🏆 Snack 4
+Crea un generatore di funzioni creaTimer
+Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!". */
+
+function creaTimer(){
+
+  setTimeout( () =>{
+    console.log("time it's over")
+  }, 1000)
+
+}
+
+creaTimer()
